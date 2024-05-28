f9177c98ddcf0198745356b7f297293ce5ee2d7f
davidecondoluci <151521993+davidecondoluci@users.noreply.github.com>
2024-05-28 16:30:39 +0200

    styling first visualization apparence

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Search from "./components/Search";
 import Card from "./components/Card";
 
-const API_URL = "http://www.omdbapi.com/?apikey=652f2e5d";
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=652f2e5d";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const searchRef = useRef(null);
 
   useEffect(() => {
     const searchMovies = async () => {
@@ -14,8 +15,11 @@ const App = () => {
         const response = await fetch(`${API_URL}&s=${searchTerm}`);
         const data = await response.json();
         setMovies(data.Search || []);
+        searchRef.current.scrollIntoView({ behavior: "smooth" });
+        document.body.classList.remove("overflow-hidden");
       } else {
         setMovies([]);
+        document.body.classList.add("overflow-hidden");
       }
     };
 
@@ -27,12 +31,29 @@ const App = () => {
   };
 
   return (
-    <div className="flex flex-col">
-      <header className="sticky top-0 bg-white">
-        <h1 className="text-2xl font-bold">Title</h1>
-        <Search handleSearchInput={handleSearchInput} />
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      <header className="w-1/2 text-center p-4">
+        <h1 className="text-2xl font-bold">Welcome to Movie Search App</h1>
+        <p>
+          This application allows you to search any type of movies. I use the
+          OMDB API to fetch the data and React to create the app. Simply type
+          the name of the movie in the search bar below and get all the
+          information you need.
+        </p>
       </header>
-      <main className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-8">
+      <div
+        ref={searchRef}
+        className="sticky top-0 bg-white w-full flex justify-center mb-4"
+      >
+        <Search handleSearchInput={handleSearchInput} />
+      </div>
+      <main
+        className={`grid ${
+          movies.length === 0
+            ? "hidden"
+            : "grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 w-full p-4"
+        }`}
+      >
         {movies.map((movie, index) => (
           <Card key={index} movie={movie} />
         ))}
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -14,12 +14,6 @@ function Search({ handleSearchInput }) {
           placeholder="Search for a Movie..."
           onChange={handleSearchInput}
         />
-        <button
-          className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
-          type="submit"
-        >
-          Search
-        </button>
       </div>
     </form>
   );
