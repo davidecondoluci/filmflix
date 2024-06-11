92b8c0507aa7f21c96e6ac50f4e0f3af227cac8b
Giammarco <giammarcogaudenzi@gmail.com>
2024-06-11 12:05:37 +0200

    added login and register routes

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,22 @@ import Layout from "./components/Layout";
 import Archive from "./pages/Archive";
 import Wishlist from "./pages/Wishlist";
 
+import { AuthProvider } from "./hooks/useAuth";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Archive />} />
-        <Route path="/wishlist" element={<Wishlist />} />
-      </Route>
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Archive />} />
+          <Route path="/wishlist" element={<Wishlist />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
+      </Routes>
+    </AuthProvider>
   );
 };
 
