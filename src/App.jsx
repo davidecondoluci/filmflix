522f2dd564d33e304be23996a12c8d91c91bf709
Giammarco <giammarcogaudenzi@gmail.com>
2024-06-11 11:18:33 +0200

    created basic wishlist page component

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 
 import Archive from "./pages/Archive";
+import Wishlist from "./pages/Wishlist";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Archive />} />
+      <Route path="/wishlist" element={<Wishlist />} />
     </Routes>
   );
 };
