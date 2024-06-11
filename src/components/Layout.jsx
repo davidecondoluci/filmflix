c6053338de279238f8b645d7f8557fd561884455
Giammarco <giammarcogaudenzi@gmail.com>
2024-06-11 11:34:08 +0200

    more links on navigation component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,9 +3,15 @@ import { Outlet } from "react-router-dom";
 const Layout = () => {
   return (
     <div className="border-8 border-blue-500 min-h-screen">
-      <div className="flex py-2 px-4 border-b border-black justify-center gap-x-2">
-        <a href="/">Archive</a>
-        <a href="/wishlist/">Wishlist</a>
+      <div className="flex py-2 px-4 border-b border-black justify-between">
+        <div className="flex items-center gap-x-2">
+          <a href="/">Archive</a>
+          <a href="/wishlist/">Wishlist</a>
+        </div>
+        <div className="flex items-center gap-x-2">
+          <a href="/login">Login</a>
+          <a href="/register">Register</a>
+        </div>
       </div>
       <Outlet />
     </div>
