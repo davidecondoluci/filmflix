7022d9afae0ffb3c8480ac97e4035969af213ca1
Giammarco <giammarcogaudenzi@gmail.com>
2024-06-11 11:16:52 +0200

    implemented basic router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import "./App.css";
+
+import { Route, Routes } from "react-router-dom";
+
 import Archive from "./pages/Archive";
 
 const App = () => {
-  return <Archive />;
+  return (
+    <Routes>
+      <Route path="/" element={<Archive />} />
+    </Routes>
+  );
 };
 
 export default App;
