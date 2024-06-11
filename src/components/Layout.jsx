9b63aecec320ea2ea616b8d387be7757016e1642
Giammarco <giammarcogaudenzi@gmail.com>
2024-06-11 11:30:03 +0200

    basic navigation links

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,6 +3,10 @@ import { Outlet } from "react-router-dom";
 const Layout = () => {
   return (
     <div className="border-8 border-blue-500 min-h-screen">
+      <div className="flex py-2 px-4 border-b border-black justify-center gap-x-2">
+        <a href="/">Archive</a>
+        <a href="/wishlist/">Wishlist</a>
+      </div>
       <Outlet />
     </div>
   );
