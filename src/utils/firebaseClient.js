3fae5313f128fa4358dde1ad1f78f46eb735bc90
davidecondoluci <151521993+davidecondoluci@users.noreply.github.com>
2025-02-02 01:12:41 +0100

    fixed firebase

--- src/utils/firebaseClient.js
+++ src/utils/firebaseClient.js
@@ -1,8 +1,10 @@
+// Importa le funzioni necessarie dal SDK di Firebase
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 
-// Configurazione Firebase
+// La tua configurazione Firebase
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -10,11 +12,14 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Inizializza Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
+const auth = getAuth(app); // Ottieni l'istanza di auth
+const db = getFirestore(app); // Ottieni l'istanza di Firestore
+const analytics = getAnalytics(app); // Se usi Google Analytics
 
-export { auth, db };
+// Esporta l'istanza di Firestore (db) e di Auth
+export { auth, db, analytics };
