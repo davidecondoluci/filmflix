a76a9c2e53c1b801f0208c2e529b5a631e9789a1
davidecondoluci <151521993+davidecondoluci@users.noreply.github.com>
2025-02-03 10:23:10 +0100

    trying to fix website

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist", // Assicurati che il build finisca qui
+  },
+  server: {
+    historyApiFallback: true, // Serve il file index.html per React Router
+  },
 });
